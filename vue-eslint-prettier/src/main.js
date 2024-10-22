a841bb07cfc91049dd70d0644884c7ad8bef870b
LifeIsOne <didtmdgh258@gmail.com>
2024-10-22 21:26:53 +0900

    [FEAT, REFACTORING] ESLint와 Prettier 설정, 프로젝트 리팩토링

--- vue-eslint-prettier/src/main.js
+++ vue-eslint-prettier/src/main.js
@@ -1,7 +1,7 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
-console.log("안녕하세요")
+createApp(App).mount('#app');
+console.log('안녕하세요');
