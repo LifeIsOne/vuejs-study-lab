d1452dc43906137038cf94a92f5b1a7d6495cdc9
LifeIsOne <didtmdgh258@gmail.com>
2024-10-22 21:50:23 +0900

    [REFACTORING] 프로젝트 정리

--- vue-eslint-prettier/eslint.config.js
+++ vue-eslint-prettier/eslint.config.js
@@ -26,17 +26,19 @@ export default [
 		rules: {
 			'no-process-env': 'off',
 			'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+			// prettier-ignore
+			// : prettier 비활성화
 			'prettier/prettier': [
 				'error',
 				{
-					printWidth: 80,
-					tabWidth: 2,
-					useTabs: true,
-					semi: true,
-					singleQuote: true,
-					trailingComma: 'all',
-					bracketSpacing: true,
-					arrowParens: 'avoid',
+					printWidth: 80, // 줄 바꿈 할 줄의 길이
+					tabWidth: 2, 		// 들여쓰기 공백 수
+					useTabs: true, 	// 공백 대신 탭으로 들여쓰기
+					semi: true, 		// 문장의 끝에 ; 추가
+					singleQuote: true, 		// ""대신 '' 사용
+					trailingComma: 'all', // 객체, 배열 등 마지막 요소 뒤에 , 추가
+					bracketSpacing: true, // {}에 공백을 추가
+					arrowParens: 'avoid', // 화살표 함수에서 매개변수가 하나일 때 () 생략
 				},
 			],
 		},
